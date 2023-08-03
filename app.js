03646e40207cd7dc27e701dd8e762cda4e296630
NickGeek1 <Nikitafilemine1@gmail.com>
2023-08-03 12:50:39 +0300

    fiat: Добавлена обработка запроса на добавление пользователя

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT = 3000 } = process.env;
+const bodyParser = require('body-parser');
+const port = process.env.PORT || '3000';
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/mestodb';
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DATABASE_URL);
 
-app.listen(PORT, () => {
-  console.log('sdsdsdsd')
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', require('./routes/users'));
+
+app.listen(port);
---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+const User = require('../models/user');
+
+// Создание пользователя
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then(user => res.status(201).send(user))
+    .catch(() => {
+      res.status(500).send(`На сервере произошла ошибка`)
+    });
+};
+
+module.exports = {
+  createUser
+}
