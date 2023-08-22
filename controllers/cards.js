20d39add8438b3621f92213313fa5de7f1f6271a
NickGeek1 <Nikitafilemine1@gmail.com>
2023-08-23 02:04:58 +0300

    fix: autotest fixes

--- controllers/cards.js
+++ controllers/cards.js
@@ -42,7 +42,7 @@ const removeCard = (req, res, next) => {
       Card.findByIdAndRemove(cardId)
         .orFail()
         .then(() => res.status(OK).send({ message: 'Карточка успешно удалена' }))
-        .catch(err => {
+        .catch((err) => {
           switch (err.constructor) {
             case mongoose.Error.CastError:
               next(new BadRequestErr('Передан невалидный id карточки'));
